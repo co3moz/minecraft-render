06615bb9a9d44359fb671570a4a55df86a7b6aab
Doğan Derya <doganderya59@gmail.com>
2021-07-29 00:43:13 +0300

    Block filtering ability for bin

--- bin/minecraft-render.js
+++ bin/minecraft-render.js
@@ -9,12 +9,13 @@ const { Minecraft, Logger } = require('../dist');
 
 program
   .usage('<jar> [output]')
-  .option('-w, --width [width]', 'width', 1000)
-  .option('-t, --height [height]', 'height', 1000)
-  .option('-d, --distance [distance]', 'distance', 20)
-  .option('-v, --verbose', 'verbose', (v, p) => typeof v != 'undefined' ? v : (p + 1), Logger.categories.info)
-  .option('-p, --plane', 'Debugging plane and axis', 0)
-  .option('-A, --no-animation', 'Disables apng generation')
+  .option('-w, --width [width]', 'output image width', 1000)
+  .option('-t, --height [height]', 'output image height', 1000)
+  .option('-d, --distance [distance]', 'distance between camera and block', 20)
+  .option('-v, --verbose', 'increases logging level', (v, p) => typeof v != 'undefined' ? v : (p + 1), Logger.categories.info)
+  .option('-p, --plane', 'debugging plane and axis', 0)
+  .option('-A, --no-animation', 'disables apng generation')
+  .option('-f, --filter <regex>', 'regex pattern to filter blocks by name')
   .version(package.version)
   .parse(process.argv);
 
@@ -28,9 +29,9 @@ async function Main() {
   Logger.level = options.verbose;
 
   const minecraft = Minecraft.open(path.resolve(program.args[0]));
-  const blocks = await minecraft.getBlockList();
-  let i = 0;
+  const blocks = filterByRegex(options.filter, await minecraft.getBlockList());
 
+  let i = 0;
   const folder = path.resolve(program.args[1] || 'output');
 
   await mkdirp(folder);
@@ -61,6 +62,14 @@ async function Main() {
   }
 
   console.log(`Rendering completed! "${folder}"`);
+}
+
+function filterByRegex(pattern, array) {
+  if (!pattern) return array;
+
+  const regex = new RegExp(pattern);
+
+  return array.filter(block => regex.test(block.blockName));
 }
 
 Main().catch(e => console.error('Rendering failed!', e));
